e21afc6fb2b76c56fbd9472cae459c689a22b035
Rafly Inggar Deansyach <rafly.dean@gmail.com>
2025-04-10 09:04:44 +0700

    Add Response Redirect to File AddSalesOrder

---
+++ TestTechnical/Dashboard.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestTechnical
+{
+	public partial class Dashboard : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				gv_salesOrder.DataBind();
+			}
+		}
+
+		protected void btn_search_Click(object sender, EventArgs e)
+		{
+			gv_salesOrder.DataBind();
+		}
+
+		protected void btn_addNewSales_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("AddSalesOrder.aspx");
+		}
+	}
+}
